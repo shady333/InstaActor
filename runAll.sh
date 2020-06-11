03b79c0e83c1f13a3ec316c04921075cba71cc7d
oleg_dudar <oleg_dudar@epam.com>
2020-06-11 19:40:34 +0300

    refactoring

--- runAll.sh
+++ runAll.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 #InstaActor sh script
-trap ctrl_c INT
 
-function ctrl_c() {
-        echo "** Trapped CTRL-C"
-        echo "shutting down Selenium Grid in docker"
-        docker-compose -f docker-compose.yaml --compatibility down
-}
-
-#Phase 1. copy resources from git (optional)
-
-rm -rfv target
-
-#Phase 2. Start selenium grid
-echo "starting Selenium GRID in docker"
-docker-compose -f docker-compose.yaml --compatibility up -d
-
-#Phase 3. Compile application
+#Phase 1. Compile application
 echo "compile application service"
 mvn clean compile
 
-#Phase 4. Start service
+#Phase 2. Start service
 echo "starting service"
 mvn exec:java -Dexec.mainClass=phaseII.Executor -Dlog4j.configuration=file:src/main/resources/log4j.properties
