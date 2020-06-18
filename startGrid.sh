7a602f36667f118df9459509d0fee5100c85a400
oleg_dudar <oleg_dudar@epam.com>
2020-06-18 18:46:07 +0300

    Update

--- startGrid.sh
+++ startGrid.sh
@@ -2,8 +2,10 @@
 
 #Phase. Start selenium grid
 echo "starting Selenium GRID in docker"
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+  echo "PI4"
   docker-compose -f docker-compose-pi.yaml --compatibility up -d
 else
+  echo "OTHERS"
   docker-compose -f docker-compose.yaml --compatibility up -d
 fi
--- stopGrid.sh
+++ stopGrid.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #InstaActor sh script
 echo "shutting down Selenium Grid in docker"
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+  echo "PI4"
   docker-compose -f docker-compose-pi.yaml --compatibility down
 else
+  echo "OTHERS"
   docker-compose -f docker-compose.yaml --compatibility down
 fi
